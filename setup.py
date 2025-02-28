6b0d9be173ffab779f41ebe89c819bce1bc2cc34
Rishi Sahasrabuddhe <sahasrabuddhe.rishi@gmail.com>
2025-02-28 22:31:32 +1100

    make numtype compatible

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='FormsAPI',
-    version='0.20', 
-    description='A standardised Forms API used to quickly make forms.',
-    author='Rishi Sahasrabuddhe',
-    url='https://github.com/PyAPIs/FormsAPI/'
-)
