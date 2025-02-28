d9a38884b05a6215616de7ae2e921c74b2db5e3f
Rishi Sahasrabuddhe <sahasrabuddhe.rishi@gmail.com>
2025-02-28 23:14:20 +1100

    Create installer.cmd

---
+++ installer.cmd
@@ -0,0 +1,20 @@
+@echo off
+git clone https://github.com/PyAPIs/FormsAPI
+
+if %ERRORLEVEL% neq 0 ( :: Check if the git clone was successful
+    echo Git clone failed. Please check you have git installed and you have permission to clone the repository.
+    exit /b 1
+)
+
+cd FormsAPI
+
+for /f "delims=" %%f in ('dir /b /a-d') do (
+    if /i not "%%f"=="Form.py" (
+        del "%%f"
+    )
+)
+
+for /d /r %%d in (*) do rd "%%d" 2>nul
+
+timeout /t 2 >nul
+del "%~f0"
